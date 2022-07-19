95fd3930af7e0dc8508509965b9489fafefe0f3b
justinlaicy926 <justinlaicy926@gmail.com>
2022-07-19 13:34:17 -0700

    project update

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Post from './Post/Post.js'
+
+const Posts = ()=> {
+    return (
+        <>
+            <h1>POSTS</h1>
+            <Post />
+            <Post />
+        </>
+        
+    );
+}
+
+export default Posts;
