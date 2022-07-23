388b33ce92b04904d689e147db95761a6e1a2dca
justinlaicy926 <justinlaicy926@gmail.com>
2022-07-22 20:37:28 -0700

    implement redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
-
+import {useDispatch} from "react-redux"
 import memories from './images/memories.png';
 
 import Posts from "./components/Posts/Post.js"
@@ -9,6 +9,11 @@ import useStyles from "./styles.js"
 
 const App = () => {
     const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(()=> {
+        dispatch(getPosts());
+    }, [dispatch]);
 
     return (
         <Container maxidth = 'lg'>
